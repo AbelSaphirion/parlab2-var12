d5a74f5c241100ae8030296548de781accbba233
Saphirion <telikinez@gmail.com>
2022-11-24 03:09:45 +0300

    Add nonsquared test

--- test_module.cpp
+++ test_module.cpp
@@ -27,3 +27,16 @@ BOOST_AUTO_TEST_CASE(assert_double){
 	BOOST_CHECK(arr[1][1] == 0);
 	BOOST_CHECK(arr[2][1] == -2);
 }
+
+BOOST_AUTO_TEST_CASE(nonesquared){
+	vector<vector<double>> arr1 = {{1, 2, 3}, {-1, 0, 5}, {3, -2, -1}, {1, 0, 1}};
+	double arr_max1[3];
+	naperdel(4, arr1, arr_max1);
+	BOOST_CHECK(arr1[3][0] == 2);
+	BOOST_CHECK(arr_max1[2] == 10);
+	vector<vector<double>> arr2 = {{1, 2, 3, 4}, {-1, 0, 5, -33}, {3, -2, -1, 7}};
+	double arr_max2[4];
+	naperdel(4, arr2, arr_max2);
+	BOOST_CHECK(arr2[0][3] == 4);
+	BOOST_CHECK(arr_max2[3] == 7);
+}
